dd9e1d8e20cc664b81f3338bd34ef0ca1240b440
BeCutter <BeCutter@web.de>
2016-07-09 00:39:53 +0200

    Design and links

--- src/de/tum/in/dbpra/model/dao/AreaDAO.java
+++ src/de/tum/in/dbpra/model/dao/AreaDAO.java
@@ -5,81 +5,80 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 import de.tum.in.dbpra.model.bean.AreaBean;
 import de.tum.in.dbpra.model.bean.AreaListBean;
 
-public class AreaDAO extends DAO{
-	
+public class AreaDAO extends DAO {
 	public void getAreas(AreaListBean listObject) throws AreaNotFoundException, SQLException, ClassNotFoundException {
-		
+
 		String query = "SELECT * FROM Area;";
-		
+
 		Connection con = getConnection();
-		
+
 		con.setAutoCommit(false);
-		
-		PreparedStatement pstmt = con.prepareStatement(query);
-		
+
+		PreparedStatement pstmt = con.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+
 		ResultSet rs = pstmt.executeQuery();
-		
-		if(DAO.getRowCount(rs)==0) {
+
+		if (DAO.getRowCount(rs) == 0) {
 			throw new AreaNotFoundException("There are no Areas found!");
 		}
-		
-		while(rs.next())
-		{
+
+		while (rs.next()) {
 			AreaBean areabean = new AreaBean();
 			areabean.setAreaID(rs.getInt("area_id"));
 			areabean.setSize(rs.getInt("size"));
 			areabean.setLocation(rs.getInt("location"));
 			listObject.setChild(areabean);
-		} 
+		}
 		con.commit();
-		
+
 		rs.close();
 		pstmt.close();
 		con.close();
-		
+
 	}
-	
+
 	public static class AreaNotFoundException extends Throwable {
 
 		private static final long serialVersionUID = 1L;
 
-		AreaNotFoundException(String message){
+		AreaNotFoundException(String message) {
 			super(message);
 		}
 	}
-	
-	public void getAreabyID(AreaBean areabean, int AreaID) throws AreaNotFoundException, SQLException, ClassNotFoundException {
-		
-		
+
+	public void getAreabyID(AreaBean areabean, int AreaID)
+			throws AreaNotFoundException, SQLException, ClassNotFoundException {
+
 		String query = "SELECT * FROM Area Where area_id=?";
-		
+
 		Connection con = getConnection();
-		
+
 		con.setAutoCommit(false);
-		
+
 		PreparedStatement pstmt = con.prepareStatement(query);
 		pstmt.setInt(1, AreaID);
-		
+
 		ResultSet rs = pstmt.executeQuery();
-		
-		if(DAO.getRowCount(rs)==0) {
+
+		if (DAO.getRowCount(rs) == 0) {
 			throw new AreaNotFoundException("There are no Areas found!");
 		}
-		
-		while(rs.next()){
+
+		while (rs.next()) {
 			areabean.setAreaID(rs.getInt("area_id"));
 			areabean.setSize(rs.getInt("Size"));
 			areabean.setLocation(rs.getInt("location"));
-		} 
+		}
 		con.commit();
-		
+
 		rs.close();
 		pstmt.close();
-		con.close();	
+		con.close();
+
 	}
-	
+
 }
