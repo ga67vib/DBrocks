6cb75c94596ef0912ab2283f9f7a0b3911162c44
MashMax <ga67vib@mytum.de>
2016-07-10 23:34:32 +0200

    This is fucking awesome (aka register works now. Our first insert. Party111)

--- src/de/tum/in/dbpra/LoginServlet.java
+++ src/de/tum/in/dbpra/LoginServlet.java
@@ -1,6 +1,9 @@
 package de.tum.in.dbpra;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -12,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import de.tum.in.dbpra.model.bean.PersonBean;
 import de.tum.in.dbpra.model.dao.PersonDAO;
+import de.tum.in.dbpra.model.dao.RFID_TicketDAO;
+import de.tum.in.dbpra.model.dao.VisitorDAO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -37,9 +42,31 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	/**
+	 * Checks whether the user used the login or the register form.
+	 * Then calls the corresponding method.
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Post login");
+		if (request.getParameter("whoami").equals("login")){
+			System.out.println("ogin");
+			doLogin(request,response);
+		}
+		else if (request.getParameter("whoami").equals("register")){
+			System.out.println("register");
+			doRegister(request,response);
+		}
+		else{
+			request.setAttribute("error", "Post request from unkown form.");
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+		}
+	}
+	
+	/**
+	 * Reads necessary data from the login form, and then tries to log the user in.
+	 * In case of invalid mail, invalid password or exceptions the user is notified.
+	 */
+	protected void doLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String mail = request.getParameter("mail");
         String password = request.getParameter("password");
         
@@ -86,4 +113,58 @@ public class LoginServlet extends HttpServlet {
         
 	}
 
+	/**
+	 * Reads necessary data from the register form, and then tries to register a visitor.
+	 * Inserts a new Person, a new visitor and a new RFID_ticket.
+	 * In case of anything not meeting the constraints or exceptions the user is notified.
+	 */
+	protected void doRegister(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String email = request.getParameter("email");
+        String password = request.getParameter("newPassword");
+        String bday = request.getParameter("bday"); 
+        String gender = request.getParameter("gender");
+        String address = request.getParameter("address");
+	 	String phonenumber = request.getParameter("phonenumber");
+	 	boolean camper = (request.getParameter("camper")!=null);
+	 	boolean vip = (request.getParameter("vip")!=null);
+	 	String prefGenre = request.getParameter("prefGenre");
+	 	Date validFrom = Date.valueOf("2016-08-19");
+	 	Date validUntil = Date.valueOf("2016-08-21");
+	 	BigDecimal price = BigDecimal.valueOf((double) 9.99);
+			
+        PersonDAO pd = new PersonDAO();
+                
+        try{
+        	//cast the birthday to date; done here, so we have the catch block to handle it in a sensible position
+        	Date birthday = Date.valueOf(bday);
+        	//PersonDAO handles the complete insert; returns the personID of the newly inserted visitor
+        	int personID = pd.insertVisitor(firstname, lastname, email, password, birthday, gender, address, phonenumber, camper, vip, prefGenre, validFrom, validUntil, price);
+        	if(pd.isVisitor(personID)){ //check the visitor was successfully inserted
+        		//immediately login and welcome the new visitor
+        		request.getSession().setAttribute("visitor", personID);
+	            request.getRequestDispatcher("/Welcome.jsp").forward(request, response);
+        	}
+        	else{
+        		request.setAttribute("error", "Unexpected error: Insert threw no exception, but visitor does not exist.");
+                request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        	}
+        }
+        catch(SQLException e){
+        	request.setAttribute("error", "SQLException occured. Text:\n"+e.getMessage());
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        }
+        catch(ClassNotFoundException e){
+        	request.setAttribute("error", "ClassNotFoundException occured. Text:\n"+e.getMessage());
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        }
+        catch(IllegalArgumentException e){
+        	request.setAttribute("error", "IllegalArgumentException occured. You entered your birthday in an invalid format. \nText of the exception:\n"+e.getLocalizedMessage());
+            request.getRequestDispatcher("/Login.jsp").forward(request, response);
+        }
+        
+        
+	}
+	
 }
--- src/de/tum/in/dbpra/model/dao/RFID_TicketDAO.java
+++ src/de/tum/in/dbpra/model/dao/RFID_TicketDAO.java
@@ -1,6 +1,8 @@
 package de.tum.in.dbpra.model.dao;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,45 +12,43 @@ import de.tum.in.dbpra.model.bean.RFID_TicketBean;
 import de.tum.in.dbpra.model.bean.RFID_TicketListBean;
 import de.tum.in.dbpra.model.dao.AreaDAO.AreaNotFoundException;
 
-public class RFID_TicketDAO extends DAO
-{
-	
-public void getTickets(RFID_TicketListBean ticketListBean)
-{
-		
+public class RFID_TicketDAO extends DAO {
+
+	public void getTickets(RFID_TicketListBean ticketListBean) {
+
 		String query = "SELECT * FROM RFID_Ticket;";
 		try {
-		Connection con = getConnection();
-		
-		con.setAutoCommit(false);
-		
-		PreparedStatement pstmt = con.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-		
-		ResultSet rs = pstmt.executeQuery();
-		
-		
-		while(rs.next())
-		{
-			RFID_TicketBean RFIDbean = new RFID_TicketBean();
-			RFIDbean.setTicketID(rs.getInt("ticket_id"));
-			RFIDbean.setAcctBal(rs.getBigDecimal("acct_bal"));
-			RFIDbean.setCamper(rs.getBoolean("is_Camper"));
-			RFIDbean.setVIP(rs.getBoolean("is_VIP"));
-			RFIDbean.setPurchaseDate(rs.getDate("purchase_Date"));
-			RFIDbean.setValidFrom(rs.getDate("valid_From"));
-			RFIDbean.setValidUntil(rs.getDate("valid_Until"));
-			RFIDbean.setPrice(rs.getInt("price"));
-			PersonBean ownedBy = new PersonBean();
-			PersonDAO personData = new PersonDAO();
-			personData.getPersonbyID(ownedBy, rs.getInt("owned_by"));
-			RFIDbean.setOwnedBy(ownedBy);
-			ticketListBean.setChild(RFIDbean);
-		} 
-		con.commit();
-		
-		rs.close();
-		pstmt.close();
-		con.close();
+			Connection con = getConnection();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pstmt = con.prepareStatement(query,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				RFID_TicketBean RFIDbean = new RFID_TicketBean();
+				RFIDbean.setTicketID(rs.getInt("ticket_id"));
+				RFIDbean.setAcctBal(rs.getBigDecimal("acct_bal"));
+				RFIDbean.setCamper(rs.getBoolean("is_Camper"));
+				RFIDbean.setVIP(rs.getBoolean("is_VIP"));
+				RFIDbean.setPurchaseDate(rs.getDate("purchase_Date"));
+				RFIDbean.setValidFrom(rs.getDate("valid_From"));
+				RFIDbean.setValidUntil(rs.getDate("valid_Until"));
+				RFIDbean.setPrice(rs.getInt("price"));
+				PersonBean ownedBy = new PersonBean();
+				PersonDAO personData = new PersonDAO();
+				personData.getPersonbyID(ownedBy, rs.getInt("owned_by"));
+				RFIDbean.setOwnedBy(ownedBy);
+				ticketListBean.setChild(RFIDbean);
+			}
+			con.commit();
+
+			rs.close();
+			pstmt.close();
+			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -56,57 +56,81 @@ public void getTickets(RFID_TicketListBean ticketListBean)
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
-public void getTicketbyID(RFID_TicketBean RFIDbean, Integer id)
-{
-String query = "SELECT * FROM RFID_Ticket Where ticket_id= ?;";
-	
-	Connection con;
-	try {
-		con = getConnection();
-	
-	con.setAutoCommit(false);
+	public void getTicketbyID(RFID_TicketBean RFIDbean, Integer id) {
+		String query = "SELECT * FROM RFID_Ticket Where ticket_id= ?;";
 
-	PreparedStatement pstmt = con.prepareStatement(query, ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
-	pstmt.setInt(1, id);
-	ResultSet rs = pstmt.executeQuery();
-	if(DAO.getRowCount(rs)==0)
-		throw new AreaNotFoundException("There were no Products found!");
-	while(rs.next())
-	{
-		RFIDbean.setTicketID(rs.getInt("ticket_id"));
-		RFIDbean.setAcctBal(rs.getBigDecimal("acct_bal"));
-		RFIDbean.setCamper(rs.getBoolean("is_Camper"));
-		RFIDbean.setVIP(rs.getBoolean("is_VIP"));
-		RFIDbean.setPurchaseDate(rs.getDate("purchase_Date"));
-		RFIDbean.setValidFrom(rs.getDate("valid_From"));
-		RFIDbean.setValidUntil(rs.getDate("valid_Until"));
-		RFIDbean.setPrice(rs.getInt("price"));
-		PersonBean ownedBy = new PersonBean();
-		PersonDAO tempa = new PersonDAO();
-		tempa.getPersonbyID(ownedBy, rs.getInt("owned_by"));
-		RFIDbean.setOwnedBy(ownedBy);
+		Connection con;
+		try {
+			con = getConnection();
+
+			con.setAutoCommit(false);
+
+			PreparedStatement pstmt = con.prepareStatement(query,
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			if (DAO.getRowCount(rs) == 0)
+				throw new AreaNotFoundException("There were no Products found!");
+			while (rs.next()) {
+				RFIDbean.setTicketID(rs.getInt("ticket_id"));
+				RFIDbean.setAcctBal(rs.getBigDecimal("acct_bal"));
+				RFIDbean.setCamper(rs.getBoolean("is_Camper"));
+				RFIDbean.setVIP(rs.getBoolean("is_VIP"));
+				RFIDbean.setPurchaseDate(rs.getDate("purchase_Date"));
+				RFIDbean.setValidFrom(rs.getDate("valid_From"));
+				RFIDbean.setValidUntil(rs.getDate("valid_Until"));
+				RFIDbean.setPrice(rs.getInt("price"));
+				PersonBean ownedBy = new PersonBean();
+				PersonDAO tempa = new PersonDAO();
+				tempa.getPersonbyID(ownedBy, rs.getInt("owned_by"));
+				RFIDbean.setOwnedBy(ownedBy);
+			}
+			con.commit();
+
+			rs.close();
+			pstmt.close();
+			con.close();
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AreaNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-		con.commit();
-	
-	rs.close();
-	pstmt.close();
-	con.close();
-	
-	} catch (ClassNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} catch (AreaNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+
+	/**
+	 * inserts the data given in the parameters into the RFID_Ticket table. Uses
+	 * the connection con for that, and does not commit. So it is one
+	 * transaction together with everything else done with con.
+	 */
+	public void insertTicket(Connection con, int personID, boolean camper,
+			boolean vip, BigDecimal price, Date validFrom, Date validUntil)
+			throws SQLException, ClassNotFoundException {
+		PreparedStatement pstmt = con
+				.prepareStatement("INSERT INTO RFID_TICKET(ticket_id,owned_by, is_Camper, is_VIP, price, valid_From, valid_Until, purchase_Date) VALUES ((SELECT MAX(ticket_id)+1 FROM RFID_ticket),?,?,?,?,?,?,(SELECT CURRENT_DATE))");
+		pstmt.setInt(1, personID);
+		pstmt.setBoolean(2, camper);
+		pstmt.setBoolean(3, vip);
+		pstmt.setBigDecimal(4, price);
+		pstmt.setDate(5, validFrom);
+		pstmt.setDate(6, validUntil);
+
+		// insert the ticket related data
+		pstmt.executeUpdate();
+		pstmt.close();
+
+		// no commit, since that is done by the calling method
+
 	}
-	
-}
-	
 
 }
