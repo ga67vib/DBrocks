1ce92ceb10e453837ddde9601e257878617dfcd3
Maxi Weininger <ga67vib@mytum.de>
2016-07-13 18:53:46 +0200

    Organization basically working...

--- src/de/tum/in/dbpra/OrganizationServlet.java
+++ src/de/tum/in/dbpra/OrganizationServlet.java
@@ -34,19 +34,19 @@ public class OrganizationServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
-			request.setAttribute("singleStaff", "Yep"); // to reuse the jsp for
-														// all notes, we set
-														// this attribute
-			int person_id = Integer.parseInt(request.getSession().getAttribute("staff").toString());
-			NoteListBean nlb = new NoteListBean();
-			NoteDAO nd = new NoteDAO();
-			nd.getNotesbyPersonID(nlb, person_id);
-			request.setAttribute("bean", nlb);
+			if(request.getSession().getAttribute("staff")!=null){ //if staff, display something; if not: Empty page
+				request.setAttribute("singleStaff", "Yep"); // to reuse the jsp for all notes, we set this attribute
+				int person_id = Integer.parseInt(request.getSession().getAttribute("staff").toString());
+				NoteListBean nlb = new NoteListBean();
+				NoteDAO nd = new NoteDAO();
+				nd.getNotesbyPersonID(nlb, person_id);
+				request.setAttribute("bean", nlb);
+			}
 		} catch (Throwable e) {
 			e.printStackTrace();
 			request.setAttribute("error", e.toString() + e.getMessage());
 		}
-		RequestDispatcher dispatcher = request.getRequestDispatcher("allNote.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/allNote.jsp");
 		dispatcher.forward(request, response);
 	}
 
