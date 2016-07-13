fe1ec4edc8362a36c3e694308c460a5006580891
Maxi Weininger <ga67vib@mytum.de>
2016-07-13 16:38:18 +0200

    Fix the shit

--- src/de/tum/in/dbpra/Config.java
+++ src/de/tum/in/dbpra/Config.java
@@ -6,14 +6,8 @@ public class Config {
 	public static final class Database {
 		public static final String HOST = "localhost";
 		public static final String PORT = "5432";
-<<<<<<< HEAD
-		public static final String DB = "festival";
-		public static final String USER = "postgres";
-		public static final String PASS = "Gromic3105";
-=======
 		public static final String DB = "DBrocks";
 		public static final String USER = "mashmax";
 		public static final String PASS = "test";
->>>>>>> 11b79b2d4ef9688a4ba85b71f415a11ff5ab15e2
 	}
 }
