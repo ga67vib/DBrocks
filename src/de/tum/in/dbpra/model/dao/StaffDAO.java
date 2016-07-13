0ccb666ca77ea891591b9acb414ef94075cbe6e8
Arnold <arnibit@googlemail.com>
2016-07-13 15:03:57 +0200

    StageDAO commented

--- src/de/tum/in/dbpra/model/dao/StaffDAO.java
+++ src/de/tum/in/dbpra/model/dao/StaffDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import de.tum.in.dbpra.model.bean.PersonBean;
 import de.tum.in.dbpra.model.bean.StaffBean;
 import de.tum.in.dbpra.model.bean.StaffListBean;
